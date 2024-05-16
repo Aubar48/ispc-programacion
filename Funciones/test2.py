5d3d1f037d12f62548ad15ed5b2cfffb977ea91b
“Nahuel <nnahuell.gb@hotmail.com>
2024-05-16 04:58:53 -0300

    test y correcion

---
+++ Funciones/test2.py
@@ -0,0 +1,8 @@
+def imprimir_argumentos(*args):
+    for arg in args:
+        print(arg)
+
+# Llamada a la función con diferentes argumentos
+imprimir_argumentos(1, 2, 3)
+imprimir_argumentos('a', 'b', 'c', 'd')
+imprimir_argumentos(True, False)
